6b0016615841cbbef8f7366564c128ccb958f0b4
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 13:34:53 +0700

    js lab 36

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// write code here
+let score = prompt("Enter your score : ");
+let grade;
+
+grade = (score >= 80) ? 'A' : (score >= 70) ? 'B' : (score >= 60) ? 'C' : (score >= 50) ? 'D' : 'F' ;
+alert(grade)
